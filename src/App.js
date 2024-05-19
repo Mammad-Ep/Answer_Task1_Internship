c5ad6bec8825c8869998d0eed185581e707051f8
Mammad Ep <mamad_en@yahoo.com>
2024-05-19 09:39:34 +0330

    add&commit index and App file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import Foods from './components/Foods';
 
-class App extends Component {
+// // Task1
+import { Link, Route, Routes, Navigate, BrowserRouter } from 'react-router-dom';
+import RickMortyList from './components/RickMortyList';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import Character from './components/Character';
+// __________________________________________________________________________________
 
-    render() {
-        return (
+// // Task1
 
-            <>
-                <Foods />
-            </>
-        );
-    }
+function App() {
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<Provider store={store}><RickMortyList /></Provider>}></Route>
+
+                <Route path={`character/:id/:page`} element={<Provider store={store}><Character /></Provider>}></Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
+// ---------------------------------------------------------------------------
+
